059839abc830ba00f99e8df617eb28a021638839
Klas-Norling <klasaman99@hotmail.com>
2023-03-03 23:01:47 +0100

    Shoppingbasket done, place order done, post item done

--- src/database/userQueries.py
+++ src/database/userQueries.py
@@ -6,22 +6,21 @@ class User:
         self.mydb = mysql.connector.connect(host="utbweb.its.ltu.se", user="19990730", passwd="19990730", database="db19990730")
         self.mycursor = self.mydb.cursor()
 
-    def add_item_shoppingbasket(self, furnitureid:int, customerid:int):
-        sql = "INSERT INTO ShoppingBasket (FurnitureID, CustomerID) VALUES (%s, %s)"
-        val = (furnitureid, customerid)
-        self.mycursor.execute(sql, val)
-        self.mydb.commimt()
-
-
-    def remove_item_shoppingbasket(self, furnitureid:int, customerid:int):
-        sql = "DELETE FROM ShoppingBasket WHERE = (%d, %d)"
-        val = (furnitureid, customerid)
+    def add_item_shoppingbasket(self, furniture, price, furnitureid:int, customerid:int):
+        sql = "INSERT INTO ShoppingBasket (Furniture, Price, CustomerID, FurnitureID) VALUES (%s, %s, %s, %s)"
+        val = (furniture, price, customerid, furnitureid)
         self.mycursor.execute(sql, val)
         self.mydb.commit()
 
 
+    def remove_item_shoppingbasket(self, furnitureid:int, customerid:int):
+        sql_delete_query = """DELETE FROM ShoppingBasket WHERE CustomerID={} AND FurnitureID={}""".format(customerid, furnitureid)
+        self.mycursor.execute(sql_delete_query)
+        self.mydb.commit()
+
+
     def place_order(self, customerid:int, furnitureid:int, city:str, address:str, last_name:str, first_name:str):
-        sql = "INSERT INTO Orders (CustomerID, FurnitureID, City, Address, LastName, FirstName) VALUES (%s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO Orders (CustomerID, Furniture, CustomerID, LastName, FirstName, City) VALUES (%s, %s, %s, %s, %s, %s)"
         val = (customerid, furnitureid, city, address, last_name, first_name)
         self.mycursor.execute(sql, val)
         self.mydb.commit()
@@ -34,12 +33,24 @@ class User:
         pass
 
 
-    def post_item(self):
-        pass
+    def post_item(self, sellerid, price, furnitureid):
+        sql = "INSERT INTO Assets (SellerID, Price, FurnitureID) VALUES (%s, %s, %s)"
+        val = (sellerid, price, furnitureid)
+        self.mycursor.execute(sql, val)
+        self.mydb.commit()
 
     def remove_item(self, furnitureid:int):
         pass
 
-
     def create_user(self, last_name:str, first_name:str, city:str, address:str):
-        pass
+        sql = "INSERT INTO Customer (CustomerID, LastName, FirstName, City, Address) VALUES (%s, %s, %s, %s, %s)"
+        val = (1, "Norling", "Emil", "Sandviken", "Professors")
+        self.mycursor.execute(sql, val)
+        self.mydb.commit()
+
+
+user = User()
+#user.create_user("klas", "hej", "då", "lala")
+#user.add_item_shoppingbasket("table", 500, 1, 1)
+user.remove_item_shoppingbasket(1, 1)
+#user.post_item(1, 500, 1)
