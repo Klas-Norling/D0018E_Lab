d25b69968a018b2f0081c9a093b1c04991e8fc7c
Klas-Norling <klasaman99@hotmail.com>
2023-03-03 22:44:38 +0100

    Finished all admin queries

--- src/database/adminQueries.py
+++ src/database/adminQueries.py
@@ -5,30 +5,30 @@ class Admin():
         self.mydb = mysql.connector.connect(host="utbweb.its.ltu.se", user="19990730", passwd="19990730", database="db19990730")
         self.mycursor = self.mydb.cursor()
 
-    def remove_asset(self, furtnitureid:int):
-        #Maybe furnitureID needs to be a string
-        self.mycursor.execute("DELETE FROM Asset WHERE FurnitureID={}".format(furtnitureid))
+    def remove_asset(self, sellerid:int):
+        sql_delete_query = """DELETE FROM Assets WHERE SellerID = {}""".format(sellerid)
+        self.mycursor.execute(sql_delete_query)
         self.mydb.commit()
 
     def remove_customer(self, customerid:int):
-        sql = "DELETE FROM Customer WHERE CustomerID = %d"
-        id = (customerid)
-        self.mycursor.execute(sql, id)
+        sql_delete_query = """DELETE FROM Customer WHERE CustomerID = {}""".format(customerid)
+        id = customerid
+        self.mycursor.execute(sql_delete_query)
         self.mydb.commit()
 
     def remove_seller(self, sellerid:int):
-        self.mycursor.execute("DELETE FROM Seller WHERE SellerID={}".format(sellerid))
+        self.mycursor.execute("DELETE FROM Seller WHERE SellerID = {}".format(sellerid))
         self.mydb.commit()
 
     def testinsert(self):
         sql = "INSERT INTO Customer (CustomerID, LastName, FirstName, City, Address) VALUES (%s, %s, %s, %s, %s)"
-        val = (1, "Norling", "Klas", "Luleå", "Professors")
+        val = (1, "Norling", "Emil", "Sandviken", "Professors")
         self.mycursor.execute(sql, val)
         self.mydb.commit()
 
 
 admin = Admin()
-admin.remove_customer(1)
+admin.remove_customer(2)
 
 
   
